c8195e1148f53fd2406eec74f5f2711a4dbf7210
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-09 17:23:06 +0300

    finish preparing and strat working on header sec...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import Header from "./components/header/header";
+import Hero from "./components/hero/Hero";
+import Main from "./components/main/Main";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/contact/footer/Footer";
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="container">
+      <Header />
+      <div className="divider" />
+      <Hero />
+      <div className="divider" />
+      <Main />
+      <div className="divider" />
+      <Contact />
+      <div className="divider" />
+
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
