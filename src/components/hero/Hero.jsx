a51ed2113338b9032544dbbc98de9a2fd9916643
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-14 17:44:04 +0300

    workin on hero section...

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -3,7 +3,17 @@ import './hero.css'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <section className='hero flex'>
+      <div className="left-section">
+        <div className="parent-avatar">
+          <img src="./a (138).jpg" className='avatar' alt="my-photo" />
+          <span className='icon-verified' />
+        </div>
+      </div>
+      <div className="right-section animation">
+        animation
+        </div>
+    </section>
   )
 }
 
