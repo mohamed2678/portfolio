591ce1cca642527d1b578eec04600cf723e2ac17
mohamed2678 <mohamedgalal2729@gmail.com>
2025-09-16 17:22:53 +0300

    finish from hero section and edit some responsive design issue

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,31 @@
-import React from 'react'
-import './hero.css'
+import React from "react";
+import "./hero.css";
 
 function Hero() {
   return (
-    <section className='hero flex'>
+    <section className="hero flex">
       <div className="left-section">
         <div className="parent-avatar">
-          <img src="./a (138).jpg" className='avatar' alt="my-photo" />
-          <span className='icon-verified' />
+          <img src="./a (138).jpg" className="avatar" alt="my-photo" />
+          <span className="icon-verified" />
+        </div>
+        <h1 className="title">software developer, & forntend developer</h1>
+        <p className="subTitle">
+          I am a software developer with a passion for creating beautiful and
+          functional web applications. I have a strong background in JavaScript,
+          React, and Node.js, and I am always eager to learn new technologies
+          and improve my skills.
+        </p>
+        <div className="all-icons flex">
+          <a href="#" target="_blank"><div className="icon icon-social-linkedin" /></a>
+          <a href="#" target="_blank"><div className="icon icon-facebook" /></a>
+          <a href="#" target="_blank"><div className="icon icon-github" /></a>
+          <a href="#" target="_blank"><div className="icon icon-instagram" /></a>
         </div>
       </div>
-      <div className="right-section animation">
-        animation
-        </div>
+      <div className="right-section animation">animation</div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
